7625039a43b8ce4298f20e0ef318294de179950e
Yogitha R <yogitharamu30@gmail.com>
2026-08-28 14:21:47 +0530

    Add Button Click Counter Java program

---
+++ 3.d
@@ -0,0 +1,38 @@
+Button Click Counter using Event Handling
+  
+Question: Write a Java program to count the number of button clicks using ActionListener.
+
+Program:
+
+import java.awt.*;
+import java.awt.event.*;
+public class ButtonClickCounter extends Frame implements ActionListener {
+int count = 0;
+Label label;
+Button button;
+ButtonClickCounter() {
+label = new Label("Button clicked 0 times");
+label.setBounds(50, 50, 200, 30);
+button = new Button("Click Me");
+button.setBounds(50, 100, 100, 30);
+button.addActionListener(this);
+add(label);
+add(button);
+setSize(300, 200);
+setLayout(null);
+setTitle("Button Click Counter");
+setVisible(true);
+addWindowListener(new WindowAdapter() {
+public void windowClosing(WindowEvent e) {
+System.exit(0);
+}
+});
+}
+public void actionPerformed(ActionEvent e) {
+count++;
+label.setText("Button clicked " + count + " times");
+}
+public static void main(String[] args) {
+new ButtonClickCounter();
+}
+}
